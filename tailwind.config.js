f816a71587a9655f05b633de78d4dfdaaee9beb6
ahp-sooyaa <sooyaa894@gmail.com>
2021-11-25 14:50:34 +0630

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,21 @@ module.exports = {
       },
       letterSpacing: {
         '2.7ls': '0.16875em',
+        '2.36ls': '0.1475em',
         '4.75ls': '0.29685em'
+      },
+      height: {
+        '532': '33.25em'
+      },
+
+      width: {
+        '515': '32.1875em'
       }
     },
     fontFamily: {
       'bellefair': ['Bellefair', 'Helvetica', 'Arial', 'sans-serif'],
-      'barlow': ['"Barlow Condensed"'],
+      'barlow': ['"Barlow"'],
+      'barlow-condensed': ['"Barlow Condensed"'],
     }
   },
   variants: {
